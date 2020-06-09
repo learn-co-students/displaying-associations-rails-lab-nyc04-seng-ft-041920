52feddf6be17c9e71e283b93c240ab7ecdc19f63
aleksarad <alexa.radevich1@gmail.com>
2020-06-09 19:52:19 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+Artist.create(name:"Nicki Minaj")
+Artist.create(name:"Mariah Carey")
+
+Song.create(title: "Chun-Li", artist_id: 1)
+Song.create(title: "Superbass", artist_id: 1)
+
+Song.create(title: "We Belong Together", artist_id: 2)
+Song.create(title: "Caution", artist_id: 2)
+
+
+
+
+
+
